4586f4adedf4cc75223b11d31184d3e287252894
Nursalim <nursalim.lsr@gmail.com>
2021-09-05 16:55:38 +0700

    belajar-python

---
+++ 05_data_type/set_tipe_data_variable.py
@@ -0,0 +1,55 @@
+nama = str("Nursalim")
+print(nama)
+print(type(nama))
+
+umur = int(30)
+print(umur)
+print(type(umur))
+
+gaji = float(3000000.0)
+print(gaji)
+print(type(gaji))
+
+complex_type = complex(5j)
+print(complex_type)
+print(type(complex_type))
+
+buah_list = list(("Apel", "Jeruk", "Strawbery"))
+print(buah_list)
+print(type(buah_list))
+
+buah_tuple = tuple(("Apel", "Jeruk", "Strawbery"))
+print(buah_tuple)
+print(type(buah_tuple))
+
+my_range = range(5)
+print(my_range)
+print(type(my_range))
+
+mahasiswa_dict = dict(nama = "Nursalim", jurusan = "Teknik Informatika", umur = 30)
+print(mahasiswa_dict)
+print(type(mahasiswa_dict))
+
+buah_set = set(("Apel", "Jeruk", "Strawbery"))
+print(buah_set)
+print(type(buah_set))
+
+buah_frozenset = frozenset(("Apel", "Jeruk", "Strawbery"))
+print(buah_frozenset)
+print(type(buah_frozenset))
+
+sudah_menikah = bool(True)
+print(sudah_menikah)
+print(type(sudah_menikah))
+
+hello_byte = bytes(5)
+print(hello_byte)
+print(type(hello_byte))
+
+my_bytearray = bytearray(5)
+print(my_bytearray)
+print(type(my_bytearray))
+
+my_memoryview = memoryview(bytearray(5))
+print(my_memoryview)
+print(type(my_memoryview))
