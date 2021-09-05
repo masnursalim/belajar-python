34e5e7f8b12cd3f696069775ebe99e9bc230a337
Nursalim <nursalim.lsr@gmail.com>
2021-09-05 15:05:33 +0700

    belajar-python

---
+++ 04_variables/05_global_variables/global_keyword.py
@@ -0,0 +1,18 @@
+def display():
+  global x
+  x = "Fantastis"
+
+display()
+
+print("Python merupakan bahasa pemrograman yang " + x)
+
+
+jurusan = "Sastra Indonesia"
+
+def tampil_jurusan():
+    global jurusan
+    jurusan = "Teknik Informatika"
+
+tampil_jurusan()
+
+print(jurusan)
---
+++ 04_variables/05_global_variables/local_variable.py
@@ -0,0 +1,8 @@
+x = "Menyenangkan"
+
+def display():
+    x = "Fantastis"
+    print("Belajar Bahasa Pemrograman Python "+x)
+
+display()
+print("Belajar Bahasa Pemrograman Python "+x)
