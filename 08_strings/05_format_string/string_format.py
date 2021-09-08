a29daddd3a756b9eac7b534268bf445d60640b3d
Nursalim <nursalim.lsr@gmail.com>
2021-09-08 10:04:19 +0700

    belajar-python

---
+++ 08_strings/05_format_string/string_format.py
@@ -0,0 +1,8 @@
+qty = 5
+txt = "Saya membeli mangga {} buah"
+print(txt.format(qty))
+
+# tambah variable
+harga = 15000
+txt = "Saya membeli mangga {} buah dengan harga {} rupiah"
+print(txt.format(qty, harga))
---
+++ 08_strings/06_escape_character/string_escape.py
@@ -0,0 +1,7 @@
+# menampilkan double quotes
+txt = "Selamat datang di blog \"masnursalim.com\" kawan."
+print(txt)
+
+# menampilkan single quote
+txt = "It\'s OK dude.."
+print(txt)
