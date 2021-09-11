bb1b0d32b802b33ef63345cb6ba53a8ffb42b71b
Nursalim <nursalim.lsr@gmail.com>
2021-09-11 15:41:46 +0700

    belajar-python

---
+++ 10_operators/operator_membership.py
@@ -0,0 +1,7 @@
+x = ["apel", "manggis", "jeruk"]
+
+# bernilai True karena manggis anggota dari x
+print("manggis" in x)
+
+# bernilai True karena pisang bukan anggota dari x
+print("pisang" not in x)
