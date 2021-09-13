76c5b9bcbdd2c0c2aa67ccfbb26507b5ccbe2217
Nursalim <nursalim.lsr@gmail.com>
2021-09-14 06:38:55 +0700

    belajar-python

--- 16_while_loop/while_loop_continue.py
+++ 16_while_loop/while_loop_continue.py
@@ -1,6 +1,18 @@
-i = 1
-while i <= 10:
-  print(i)
-  if i%2 == 0:
-      continue
+# mencetak angka ganjil
+print("Angka ganjil 1 s/d 10")
+i = 0
+while i < 10:
   i += 1
+  if i % 2 == 0:
+    continue
+  print(i)
+
+# mencetak angka ganjil
+print()
+print("Angka genap 1 s/d 10")
+i = 0
+while i < 10:
+  i += 1
+  if i % 2 != 0:
+    continue
+  print(i)
